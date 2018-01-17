53478e7fd3e5261eb4674257b61b7bcd5a98d23c
whenofficial <fsmitchm@gmail.com>
2018-01-18 00:48:36 +0400

    updated index.php/attempted loader

--- index.php
+++ index.php
@@ -1,4 +1,24 @@
 <?php
 
 ?>
-<!-- index lines here, replaces INDEX.HTML but is basically the same -->
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>French vACC HQ</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
+	<link rel="stylesheet" href="styles/css/login.css" />
+	<link rel="icon" href="styles/images/favicon.ico" type="image/x-icon" />
+</head>
+<body>
+	<div class="body"></div>
+		<div class="grad"></div>
+			<div class="header">
+				<div>VAT<span class="blue"><strong>FRANCE</strong></span></div>
+			</div>
+			<br>
+			<div class="login"><br><br>
+				<input type="button" value="Login with VATSIM" onclick="location.href='library/sso/Authentication.php'">
+			</div>
+	</div>
+</html>
