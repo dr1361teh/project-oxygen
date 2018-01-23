b556437d3df5e94d3285f8a3c3a4421a7585072f
whenofficial <fsmitchm@gmail.com>
2018-01-24 00:03:36 +0400

    Created errors page

---
+++ modules/error.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+if(!isset($_SESSION['errcode'])){
+    $_SESSION['errcode'] = '0x003';
+    $_SESSION['errmsg'] = 'No valid or set error code is set.';
+    $_SESSION['errdesc'] = 'You are trying to access error.php with no error code to display';
+} elseif(!isset($_SESSION['errmsg'])){
+    $_SESSION['errcode'] = '0x004';
+    $_SESSION['errmsg'] = 'No valid or set error message is set.';
+    $_SESSION['errdesc'] = 'You are trying to access error.php with no error message to display';
+} elseif(!isset($_SESSION['errdesc'])){
+    $_SESSION['errcode'] = '0x005';
+    $_SESSION['errmsg'] = 'No valid or set error description is set.';
+    $_SESSION['errdesc'] = 'You are trying to access error.php with no error description to display';
+}
+?>
+
+<link rel="stylesheet" href="../styles/css/AdminLTE.min.css">
+<h2 style='color:red'>Error</h2>
+    <h3>ERRCODE: 
+        <strong><?php echo $_SESSION['errcode'];?></strong>
+    </h3>
+    <p><?php echo $_SESSION['errmsg'];?>
+        <strong><?php echo $_SESSION['errdesc'];?></strong>
+    </p>
+    <p>Do not reload this page!</p>
+    <a onclick="window.history.go(-1); return false;">Go Back</a>
+ <?php
+        if($_SESSION['errcode'] === '0x002'){
+            session_destroy();
+        }
+    unset($_SESSION['errcode']);
+    unset($_SESSION['errmsg']);
+    unset($_SESSION['errdesc']);
+ ?>
