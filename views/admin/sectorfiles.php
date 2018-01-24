3abc7f41a508d3fb7e94b8ecb2638a028264411a
ATCKL <33000075+ATCKL@users.noreply.github.com>
2018-01-24 21:08:05 +0800

    Finsihed coding page.

--- views/admin/sectorfiles.php
+++ views/admin/sectorfiles.php
@@ -35,13 +35,71 @@ require('header.php');
           <!-- /.box -->
         </section>
         <!-- /.content -->
+       <!-- /.row -->
+       <div class="row">
+        <div class="col-xs-12">
+          <div class="box">
+            <div class="box-header">
+              <h3 class="box-title">Sector Downloads</h3>
+
+              <div class="box-tools">
+                <div class="input-group input-group-sm" style="width: 135px;">
+                  <input type="text" name="table_search" class="form-control pull-right" placeholder="Search">
+
+                  <div class="input-group-btn">
+                    <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body table-responsive no-padding">
+              <table class="table table-hover">
+                <tr>
+                  <th>AIRAC Cycle</th>
+                  <th>Uploaded by</th>
+                  <th>Date</th>
+                  <th>Status</th>
+                  <th>Reason</th>
+                </tr>
+                <tr>
+                  <td>1713</td>
+                  <td>ATC Director</td>
+                  <td>13/01/2018</td>
+                  <td><span class="label label-success">Approved</span></td>
+                  <td>AIRAC Cycle 1713. </td>
+                </tr>
+                <tr>
+                  <td>1712</td>
+                  <td>ATC Director</td>
+                  <td>24/012/2017</td>
+                  <td><span class="label label-danger">Outdated!</span></td>
+                  <td>AIRAC Cycle 1712. Major Paris sector Overhaul.</td>
+                </tr>
+                <tr>
+                  <td>1711</td>
+                  <td>ATC Director</td>
+                  <td>24/11/2017</td>
+                  <td><span class="label label-danger">Outdated!</span></td>
+                  <td>AIRAC Cycle 1711. Added some more usefull Alias.</td>
+                </tr>
+                <tr>
+                  <td>1710</td>
+                  <td>ATC Director</td>
+                  <td>24/10/2017</td>
+                  <td><span class="label label-danger">Outdated!</span></td>
+                  <td>AIRAC Cycle 1710. Updated to the newest AIRAC.</td>
+                </tr>
+              </table>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
       </div>
-      <!-- /.container -->
-    </div>
-
-
-
-
+    </section>
+    <!-- /.content -->
+  </div>
 
 
 <?php
