a848223c299de0d4b3c83973c8e4249a702e05da
whenofficial <fsmitchm@gmail.com>
2018-01-24 12:05:10 +0400

    Added title feature

--- views/admin/header.php
+++ views/admin/header.php
@@ -5,7 +5,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 2 | Top Navigation</title>
+  <title><?php if(isset($title)){ echo $title; } else { echo 'VATFRANCE'; }?></title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
