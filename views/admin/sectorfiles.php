5b0741fd9ac051e96fb76f55864873f9ff8a6e23
ATCKL <33000075+ATCKL@users.noreply.github.com>
2018-01-24 17:11:48 +0800

    Fixed directory problem with sectorfiles.php

---
+++ views/admin/sectorfiles.php
@@ -0,0 +1,27 @@
+<?php
+$title = 'SectorFiles';
+
+?>
+<?php
+require('header.php');
+?>
+
+<html>
+<ul class="treeview-menu">
+            <li class="active"><a href="simple.html"><i class="fa fa-circle-o"></i> Simple tables</a></li>
+            <li><a href="data.html"><i class="fa fa-circle-o"></i> Data tables</a></li>
+          </ul>
+        </li>
+
+
+
+
+
+
+
+
+
+
+<?php
+require('footer.php');
+?>
