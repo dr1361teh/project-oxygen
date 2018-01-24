ad0c013d220de39f3744f0cf613c024bb74d4e35
ATCKL <33000075+ATCKL@users.noreply.github.com>
2018-01-24 22:06:39 +0800

    Initial Roster creation

---
+++ views/admin/controllerroster.php
@@ -0,0 +1,254 @@
+<?php
+$title = 'controller=roster';
+
+?>
+<?php
+require('header.php');
+?>
+
+  <!-- Full Width Column -->
+  <div class="content-wrapper">
+    <div class="container">
+      <!-- Content Header (Page header) -->
+      <section class="content-header">
+        <h1>
+          HQ System
+          <small>Version PRE-APLHA 0.0.1</small>
+        </h1><br>
+        <ol class="breadcrumb">
+          <li><a href="#"><i class="fa fa-dashboard"></i> HQ</a></li>
+          <li><a href="#">ATC</a></li>
+          <li class="active">Controller Roster</li>
+        </ol>
+      </section>
+
+          <!-- Main content -->
+          
+          <div class="box box-default">
+            <div class="box-header with-border">
+              <h3 class="box-title">Restricted Roster</h3>
+            </div>
+            <div class="box-body">
+              VATFRANCE Has 2 restricted airports that includes Paris Charles De Gaulle (Roissy - CDG/LFPG) and Nice Cote D'Azur (LFMN)
+              These are the list of approved controllers for these positions.
+              If you wish to be added to the roster and get mentoring simply send a support ticket or email one of the training staff.
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </section>
+        <!-- /.content -->
+          
+       <!-- /.row -->
+       <div class="row">
+        <div class="col-xs-12">
+          <div class="box">
+            <div class="box-header">
+              <h3 class="box-title">Roster</h3>
+
+              <div class="box-tools">
+                <div class="input-group input-group-sm" style="width: 135px;">
+                  <input type="text" name="table_search" class="form-control pull-right" placeholder="Search">
+
+                  <div class="input-group-btn">
+                    <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body table-responsive no-padding">
+              <table class="table table-hover">
+                <tr>
+                <th>Name</th>
+                <th>ID</th>
+                <th>Rating</th>
+                <th>Approved by</th>
+                <th>Positions</th>
+                </tr>
+                <tr>
+                <td>Alain Verhaeghe </td>
+                <td>817811</td>
+                <td>C1</td>
+                <td>Training Staff</td>
+                <td><span class="label label-success">LFPG_DEL/GND/TWR/APP</span></td>
+                </tr>
+                <tr>
+                <td>Bernard Husson</td>
+                <td>1131230</td>
+                <td>S3</td>
+                <td>Training Staff</td>
+                <td><span class="label label-success">LFPG_DEL/GND/TWR/APP</span></td>
+                </tr>
+                <tr>
+                <td>Cathy Mariani</td>
+                <td>1183082</td>
+                <td>C3</td>
+                <td>Training Staff</td>
+                <td><span class="label label-success">LFPG_DEL/GND/TWR/APP</span></td>
+                </tr>
+                <tr>
+                <td>Chriss Klosowski </td>
+                <td>1306415</td>
+                <td>C1</td>
+                <td>Training Staff</td>
+                <td><span class="label label-success">LFPG_DEL/GND/TWR/APP</span></td>
+                </tr>
+                <tr>
+                <td>David Petit </td>
+                <td>1273992</td>
+                <td>C1</td>
+                <td>Training Staff</td>
+                <td><span class="label label-success">LFPG_DEL/GND/TWR/APP</span></td>
+                </tr>
+                <tr>
+                <td>Eric Lavorel  </td>
+                <td>1342825</td>
+                <td>S3</td>
+                <td>Training Staff</td>
+                <td><span class="label label-success">LFPG_DEL/GND/TWR/APP</span></td>
+                </tr>
+                <tr>
+                <td>Florian Rimoli</td>
+                <td>1225664</td>
+                <td>C1</td>
+                <td>Training Staff</td>
+                <td><span class="label label-success">LFPG_DEL/GND/TWR/APP</span></td>
+                </tr>
+                <tr>
+                <td>François-Xavier Obert </td>
+                <td>1086470</td>
+                <td>I1</td>
+                <td>Training Staff</td>
+                <td><span class="label label-success">LFPG_DEL/GND/TWR/APP</span></td>
+                </tr>
+                <tr>
+                <td>Jean louis Richon </td>
+                <td>922139</td>
+                <td>C1</td>
+                <td>Training Staff</td>
+                <td><span class="label label-success">LFPG_DEL/GND/TWR/APP</span></td>
+                </tr>
+                <tr>
+                <td>Jean Tirancourt </td>
+                <td>1047231</td>
+                <td>S3</td>
+                <td>Training Staff</td>
+                <td><span class="label label-success">LFPG_DEL/GND/TWR/APP</span></td>
+                </tr>
+                <tr>
+                <td>Joel Lagarrigue </td>
+                <td>1107750</td>
+                <td>C1</td>
+                <td>Training Staff</td>
+                <td><span class="label label-success">LFPG_DEL/GND/TWR/APP</span></td>
+                </tr>
+                <tr>
+                <td>Loic LEBAIN </td>
+                <td>1059663</td>
+                <td>C1</td>
+                <td>Training Staff</td>
+                <td><span class="label label-success">LFPG_DEL/GND/TWR/APP</span></td>
+                </tr>
+                <tr>
+                <td>Marc GUETRE </td>
+                <td>986422</td>
+                <td>C1</td>
+                <td>Training Staff</td>
+                <td><span class="label label-success">LFPG_DEL/GND/TWR/APP</span></td>
+                </tr>
+                <tr>
+                <td>Patrice Jouan </td>
+                <td>916063</td>
+                <td>S3</td>
+                <td>Training Staff</td>
+                <td><span class="label label-success">LFPG_DEL/GND/TWR/APP</span></td>
+                </tr>
+                <tr>
+                <td>Patrick DUFLOT</td>
+                <td>1018762</td>
+                <td>C1</td>
+                <td>Training Staff</td>
+                <td><span class="label label-success">LFPG_DEL/GND/TWR/APP</span></td>
+                </tr>
+                <tr>
+                <td>Patrick fuchez </td>
+                <td>819114</td>
+                <td>C3</td>
+                <td>Training Staff</td>
+                <td><span class="label label-success">LFPG_DEL/GND/TWR/APP</span></td>
+                </tr>
+                <tr>
+                <td>Pierre Ferran </td>
+                <td>1259058</td>
+                <td>C1</td>
+                <td>Training Staff</td>
+                <td><span class="label label-success">LFPG_DEL/GND/TWR/APP</span></td>
+                </tr>
+                <tr>
+                <td>Pierre Messager </td>
+                <td>1190244</td>
+                <td>S3</td>
+                <td>Training Staff</td>
+                <td><span class="label label-success">LFPG_DEL/GND/TWR/APP</span></td>
+                </tr>
+                <tr>
+                <td>Raphël Guillod </td>
+                <td>921950</td>
+                <td>C1</td>
+                <td>Training Staff</td>
+                <td><span class="label label-success">LFPG_DEL/GND/TWR/APP</span></td>
+                </tr>
+                <tr>
+                <td>Reda FAIZ </td>
+                <td>1171306</td>
+                <td>C1</td>
+                <td>Training Staff</td>
+                <td><span class="label label-success">LFPG_DEL/GND/TWR/APP</span></td>
+                </tr>
+                <tr>
+                <td>Samuel Lafage </td>
+                <td>1176432</td>
+                <td>C1</td>
+                <td>Training Staff</td>
+                <td><span class="label label-success">LFPG_DEL/GND/TWR/APP</span></td>
+                </tr>
+                <tr>
+                <td>Simon Pechinot </td>
+                <td>1162027</td>
+                <td>S3</td>
+                <td>Training Staff</td>
+                <td><span class="label label-success">LFPG_DEL/GND/TWR/APP</span></td>
+                </tr>
+                <tr>
+                <td>Thomas Merle </td>
+                <td>1199123</td>
+                <td>I1</td>
+                <td>Training Staff</td>
+                <td><span class="label label-success">LFPG_DEL/GND/TWR/APP</span></td>
+                </tr>
+                <tr>
+                <td>Walid AMAR </td>
+                <td>1052472</td>
+                <td>C1</td>
+                <td>Training Staff</td>
+                <td><span class="label label-success">LFPG_DEL/GND/TWR/APP</span></td>
+                </tr>
+
+
+              </table>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+      </div>
+    </section>
+    <!-- /.content -->
+  </div>
+
+
+
+<?php
+require('footer.php');
+?>
