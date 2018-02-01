3929ed8802f412ece3cc30db61939b7077d99c46
whenofficial <fsmitchm@gmail.com>
2018-02-01 17:19:59 +0400

    Checked bug

--- views/admin/header.php
+++ views/admin/header.php
@@ -1,7 +1,5 @@
 <?php
 //Header File (global for admin dashboard)
-require '../config.php';
-require '../connection.php';
 
 try {
   if (empty($_SESSION['cid'])) {
@@ -30,6 +28,8 @@ if(empty($result)){
   $_SESSION['errdesc'] = "Please contact your division director.";
   header('Location: index.php?content=error');
 }
+// require('../config.php');
+// require('../connection.php');
 
 //Create session variables
 $_SESSION['firstname'] = $result['firstname'];
@@ -48,16 +48,16 @@ $_SESSION['country']  = $result['country'];
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
-  <link rel="stylesheet" href="../views/admin/bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="views/admin/bower_components/bootstrap/dist/css/bootstrap.min.css">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="../views/admin/bower_components/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="views/admin/bower_components/font-awesome/css/font-awesome.min.css">
   <!-- Ionicons -->
-  <link rel="stylesheet" href="../views/admin/bower_components/Ionicons/css/ionicons.min.css">
+  <link rel="stylesheet" href="views/admin/bower_components/Ionicons/css/ionicons.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="../styles/css/AdminLTE.min.css">
+  <link rel="stylesheet" href="styles/css/AdminLTE.min.css">
   <!-- AdminLTE Skins. Choose a skin from the css/skins
        folder instead of downloading all of them to reduce the load. -->
-  <link rel="stylesheet" href="../styles/css/skins/_all-skins.min.css">
+  <link rel="stylesheet" href="styles/css/skins/_all-skins.min.css">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
