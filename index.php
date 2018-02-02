2ac09ca8eb89481154e0d7529bb01d30c6e60e95
whenofficial <fsmitchm@gmail.com>
2018-02-02 15:48:27 +0400

    Check for localhost

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
 session_start();
 
+//Get the router
 require('route/master.php');
+
+//Check if localhost
+if($_SERVER['REMOTE_ADDR'] === '::1' || '127.0.0.1' || 'localhost'){
+    if($_SERVER['REMOTE_ADDR'] === '::1'){
+        $ipaddr = '127.0.0.1';
+    } else {
+        $ipaddr = $_SERVER['REMOTE_ADDR'];
+    }
+    echo '<center><b>Debug</b>: Running on '.$ipaddr.' on '.$_SERVER['HTTP_USER_AGENT'].'</center>';
+}
 ?>
