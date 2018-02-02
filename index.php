8088ebe0a856fd7de3226695fdce997ee50ea43c
whenofficial <fsmitchm@gmail.com>
2018-02-02 15:37:56 +0400

    merge

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
-//Get the router
-require('route/master.php');
+session_start();
 
-//Check if accessed via localhost (testing server)
-if($_SERVER['REMOTE_ADDR'] = '0.0.0.1' || '127.0.0.1' || '::1' || 'localhost'){
-    echo '<b>Disclaimer</b>: IP address: '.$_SERVER['REMOTE_ADDR'].' (test access)';
-}
+require('route/master.php');
 ?>
