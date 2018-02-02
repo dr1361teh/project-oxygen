462e5a1fcd902c6914662a590d8a3909d79a9d6b
whenofficial <fsmitchm@gmail.com>
2018-02-02 15:32:18 +0800

    Fixed grammar errors in localhost error

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ require('route/master.php');
 
 //Check if accessed via localhost (testing server)
 if($_SERVER['REMOTE_ADDR'] = '0.0.0.1' || '127.0.0.1' || '::1' || 'localhost'){
-    echo '<b>Disclaimer</b>: This website is being run on <i>localhost</i> (test server on local machine). IP address: '.$_SERVER['REMOTE_ADDR'];
+    echo '<b>Disclaimer</b>: IP address: '.$_SERVER['REMOTE_ADDR'].' (test access)';
 }
 ?>
