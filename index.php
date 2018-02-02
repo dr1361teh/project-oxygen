43d4bdf82cee5c42b3af8b445d566b2721c3bc5a
whenofficial <fsmitchm@gmail.com>
2018-02-02 15:31:06 +0800

    Checked if localhost test machine

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
 //Get the router
 require('route/master.php');
+
+//Check if accessed via localhost (testing server)
+if($_SERVER['REMOTE_ADDR'] = '0.0.0.1' || '127.0.0.1' || '::1' || 'localhost'){
+    echo '<b>Disclaimer</b>: This website is being run on <i>localhost</i> (test server on local machine). IP address: '.$_SERVER['REMOTE_ADDR'];
+}
 ?>
