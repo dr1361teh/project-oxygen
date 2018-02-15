45792e028ccc7169e41deaf23a8098749f4cc927
whenofficial <fsmitchm@gmail.com>
2018-02-15 09:08:40 +0800

    changed admin/ to en/

--- views/en/footer.php
+++ views/en/footer.php
@@ -13,13 +13,13 @@
 <!-- ./wrapper -->
 
 <!-- jQuery 3 -->
-<script src="views/admin/bower_components/jquery/dist/jquery.min.js"></script>
+<script src="views/en/bower_components/jquery/dist/jquery.min.js"></script>
 <!-- Bootstrap 3.3.7 -->
-<script src="views/admin/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="views/en/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 <!-- SlimScroll -->
-<script src="views/admin/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<script src="views/en/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
 <!-- FastClick -->
-<script src="views/admin/bower_components/fastclick/lib/fastclick.js"></script>
+<script src="views/en/bower_components/fastclick/lib/fastclick.js"></script>
 <!-- AdminLTE App -->
 <script src="styles/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
