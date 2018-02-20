17e96e768cc04a62377ba002d71465140ca8706a
whenofficial <fsmitchm@gmail.com>
2018-02-20 16:04:24 +0400

    LEGACY

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require('library/connection.php');
 require('route/master.php');
 
 
-
+//LEGACY
 //Check if localhost
 if($_SERVER['REMOTE_ADDR'] === '::1' || '127.0.0.1' || 'localhost'){
     if($_SERVER['REMOTE_ADDR'] === '::1'){
