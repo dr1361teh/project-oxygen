2e415624d04a821f8160864fc48cb5f3b248eff2
whenofficial <fsmitchm@gmail.com>
2018-02-20 16:59:44 +0400

    db init

---
+++ library/config/confg.php
@@ -0,0 +1,12 @@
+<?php
+//Main Site Configuration
+
+//DATABASE
+$config['dbHost'] = 'localhost';
+$config['dbUser'] = 'root';
+$config['dbPass'] = '';
+$config['dbName'] = 'vatfrance';
+
+//DATA
+$config['countryAbbr'] = 'FRA'; //For VATEUD APi requests
+?>
--- library/database/mysqli.php
+++ library/database/mysqli.php
@@ -1,5 +1,10 @@
 <?php
 //MySQLi Database Init
+require('../config/config.php');
+if($sqli = new mysqli($config['dbHost'], $config['dbUser'], $config['dbPass'], $config['dbName'])){
 
+} else {
+    echo 'Unable to create a connection to the MySQLi database. Please try again later, or if you are the webmaster, please check your entries in <i>config.php</i>';
+}
 
 ?>
