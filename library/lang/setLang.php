87931017684cf6bc775f24a72d431af62bb8952a
whenofficial <fsmitchm@gmail.com>
2018-02-20 17:12:21 +0400

    language inti

---
+++ library/lang/setLang.php
@@ -0,0 +1,10 @@
+<?php
+//Set user language based on button click on header
+
+$lang = $_GET['lang'];
+
+setcookie('lang', $lang, 2147483647);
+
+header('Location: ../../index.php?content=index');
+
+?>
