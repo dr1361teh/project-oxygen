e7b0e83e62499c1889f17a1229dd6db7f9792dbd
whenofficial <fsmitchm@gmail.com>
2018-02-21 13:47:27 +0800

    require_once()

--- index.php
+++ index.php
@@ -1,18 +1,21 @@
 <?php
 session_start();
 
-//Get the router
-require('model/config/config.php');
-require('model/database/mysqli.php');
-require('router/master.php');
+//Get required files
+require_once('model/config/config.php');
+require_once('model/database/mysqli.php');
+require_once('router/master.php');
 
-//Check if localhost
+//Check the client to see if user is running server on localhost
 if($_SERVER['REMOTE_ADDR'] === '::1' || '127.0.0.1' || 'localhost'){
     if($_SERVER['REMOTE_ADDR'] === '::1'){
+        //Sets the data
         $ipaddr = '127.0.0.1';
     } else {
+        //Sets the data
         $ipaddr = $_SERVER['REMOTE_ADDR'];
     } 
+    //Echo footer for all pages
     echo '<center><b>Server</b>: Running on '.$ipaddr.' on '.$_SERVER['HTTP_USER_AGENT'].'</center>';
 }
 
